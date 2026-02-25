61196b780efaa2a1bc0cef856b233c9e75cafb6b
Hector Santandreu Martinez <hectorsm20001@gmail.com>
2026-02-25 15:34:32 +0100

    Modelos Task y Category terminados

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'completed', 'category_id'];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
